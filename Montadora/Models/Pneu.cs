022aab37e3c32cf415e427cfcbd31e76cef56369
Brunopires1999 <123516656+Brunopires1999@users.noreply.github.com>
2023-02-01 21:56:44 -0300

    teste 1.1
    montadora 2

---
+++ Montadora/Models/Pneu.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+    public class Pneu
+    {
+    public int  { get; set; }
+    public bool TWI { get; set; }
+    public string Cor { get; set; }
+    public string Marca{ get; set; }
+    public int VelociadadeMaxima { get; set; }
+    public int VelocidadeAtual { get; set; }
+    public bool Estepe { get; set; }
+    public bool Estourado { get; set; }
+   
+}
+}
